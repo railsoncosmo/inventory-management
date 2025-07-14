159ad53b533a282100b0bad8dab611a17b0c11ef
Railson Cosmo <railson_sousa10@icloud.com>
2025-07-13 21:03:35 -0300

    feat: add class for validating http requests

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -6,3 +6,27 @@ export class AppError extends Error {
     this.statusCode = statusCode
   }
 }
+
+export class BadRequestError extends AppError {
+  constructor(message: string){
+    super(message, 400 )
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string){
+    super(message, 404 )
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string){
+    super(message, 401 )
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string){
+    super(message, 403 )
+  }
+}
