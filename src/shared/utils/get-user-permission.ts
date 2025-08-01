4215ae1c9e3aa4b29ca76fc9fc5d7ed505387e8d
Railson Cosmo <railson_sousa10@icloud.com>
2025-07-31 22:47:00 -0300

    refactor: restructure layer shared and function utils

---
+++ src/shared/utils/get-user-permission.ts
@@ -0,0 +1,15 @@
+import { defineAbilityFor } from '../../auth/rbac/index'
+import { userSchema } from '../../auth/rbac/models/user'
+import { Role } from '../../auth/rbac/roles'
+
+
+export function getUserPermission(userId: string, role: Role){
+  const authUser = userSchema.parse({
+    id: userId,
+    role,
+  })
+
+  const ability = defineAbilityFor(authUser)
+
+  return ability
+}
