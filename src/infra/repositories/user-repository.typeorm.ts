6357fcf236e035bf819d4d17455c290fd3d77563
Railson Cosmo <railson_sousa10@icloud.com>
2025-08-02 22:11:49 -0300

    refactor: build mapper user persistence with user domain

--- src/infra/repositories/user-repository.typeorm.ts
+++ src/infra/repositories/user-repository.typeorm.ts
@@ -3,7 +3,6 @@ import { User as UserORM } from '../database/typeorm/entities/User'
 import { User as UserDomain } from '../../core/domain/entities/user/user.entity'
 import { UserGateway } from '../../core/domain/entities/user/user.gateway'
 import { UserRole } from '../../core/domain/enums/roles'
-import { UserMapper } from '../mappers/user.mapper'
 
 export class UserTypeormRepository implements UserGateway {
   private constructor(private readonly typeOrmClient: Repository<UserORM>) {}
@@ -33,6 +32,6 @@ export class UserTypeormRepository implements UserGateway {
   async find(email: string): Promise<UserDomain | null> {
     const user = await this.typeOrmClient.findOne({ where: { email } })
     if (!user) return null
-    return UserMapper.toDomain(user)
+    return UserDomain.withUser(user)
   }
 }
