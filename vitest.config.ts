a3fdbe3598172fcb03ac3b3e65d5d3c194238243
Railson Cosmo <railson_sousa10@icloud.com>
2025-08-24 19:59:11 -0300

    chore: add coverage for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
+    coverage: {
+      reporter: ['text', 'json'],
+      
+    },
     globals: true,
     setupFiles: ['./src/__test__/setup.ts'],
   },
